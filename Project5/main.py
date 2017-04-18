812974b48edb72d813d70cf8e4d4b3386db6d7b3
Austin Doolittle <austindoolittle@gmail.com>
2017-04-18 14:39:55 -0400

    Added cat game detection, different gamemodes

--- Project5/main.py
+++ Project5/main.py
@@ -1,10 +1,15 @@
 import argparse as ap
-from ttt import TTT, InvalidMoveException, PLAYER_1, PLAYER_2
+from ttt import TTT, InvalidMoveException, PLAYER_1, PLAYER_2, CAT_GAME
+from random import randint
 import sys
 
+PLAYER_VS_PLAYER = "PvP"
+PLAYER_VS_COMPUTER = "PvC"
+PLAYER_VS_LEARNER = "PvL"
+LEARNER_VS_COMPUTER = "LvC"
+LEARNER_VS_LEARNER = "LvL"
 
-
-def get_move():
+def get_player_move():
   while True:
     try:
       move = map(int, raw_input().split())
@@ -17,39 +22,55 @@ def get_move():
     break
   return move
 
+def get_random_move():
+  return [randint(0,2), randint(0,2)]
+
 def main(args):
   parser = ap.ArgumentParser(description="A Game of Tic Tac Toe that gradually learns how to play over time")
   parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Increase verbosity")
+  parser.add_argument("--gametype", default=PLAYER_VS_PLAYER, 
+                      choices=[PLAYER_VS_PLAYER, PLAYER_VS_COMPUTER, PLAYER_VS_LEARNER, LEARNER_VS_COMPUTER, LEARNER_VS_LEARNER])
   args = parser.parse_args(args)
 
-  game = TTT(args.verbose)
-  
-  curr_player = 1
-  while True:
-    print "Player " + ("1" if curr_player == PLAYER_1 else "2") + ", your move: "
+  if args.gametype in [PLAYER_VS_PLAYER, PLAYER_VS_COMPUTER]:
+    game = TTT(args.verbose)
+    
+    curr_player = 1
     while True:
-      move = get_move()
-      try: 
-        game.move(curr_player, move)
-      except InvalidMoveException as e:
-        print "That spot is already taken"
-        continue
-      break
 
-    print
-    game.draw_board()
-    print
+      while True:
+        print
+        print "Player " + ("1" if curr_player == PLAYER_1 else "2") + ", your move: "
+        if args.gametype == PLAYER_VS_PLAYER or curr_player == PLAYER_1:
+          move = get_player_move()
+        else:
+          move = get_random_move()
+          print str(move[0]) + " " + str(move[1])
+        try: 
+          game.move(curr_player, move)
+        except InvalidMoveException as e:
+          print "That spot is already taken"
+          continue
+        break
 
-    check = game.check_win()
-    if check == PLAYER_1:
-      print "Player 1 wins!"
-      break
-    elif check == PLAYER_2:
-      print "Player 2 wins!"
-      break
+      print
+      game.draw_board()
 
-    curr_player = -curr_player
-  
+      check = game.check_win()
+      if check == PLAYER_1:
+        print "Player 1 wins!"
+        break
+      elif check == PLAYER_2:
+        print "Player 2 wins!"
+        break
+      elif check == CAT_GAME:
+        print "It's a tie!"
+        break
+
+      curr_player = -curr_player
+  else:
+    raise NotImplementedError()
+    
 
 
 
--- Project5/ttt.py
+++ Project5/ttt.py
@@ -1,44 +1,62 @@
 import numpy as np
 
 PLAYER_1 = 1
+EMPTY = 0
 PLAYER_2 = -1
 PLAYER_1_TOKEN = 'X'
 PLAYER_2_TOKEN = 'O'
 EMPTY_TOKEN = ' '
+CAT_GAME = -2
+NO_WIN = 0
 
 
 class TTT:
 
   def __init__(self, verbose):
     #create a 3x3 array for the board
-    self.board = np.full((3,3), EMPTY_TOKEN)
+    self.board = np.full((3,3), EMPTY)
     self.verbose = verbose
 
   def draw_board(self):
-    print " " + self.board[0][0] + " | " + self.board[0][1] + " | "  + self.board[0][2]
+    temp_board = [[PLAYER_1_TOKEN if x == PLAYER_1 else PLAYER_2_TOKEN if x == PLAYER_2 else EMPTY_TOKEN for x in y] for y in self.board]
+    print " " + temp_board[0][0] + " | " + temp_board[0][1] + " | "  + temp_board[0][2]
     print "----------"
-    print " " + self.board[1][0] + " | " + self.board[1][1] + " | "  + self.board[1][2]
+    print " " + temp_board[1][0] + " | " + temp_board[1][1] + " | "  + temp_board[1][2]
     print "----------"
-    print " " + self.board[2][0] + " | " + self.board[2][1] + " | "  + self.board[2][2]
+    print " " + temp_board[2][0] + " | " + temp_board[2][1] + " | "  + temp_board[2][2]
 
   def move(self, player, move):
-    if self.board[move[0]][move[1]] != EMPTY_TOKEN:
-      raise InvalidMoveException("Position already taken by token " + self.board[move[0]][move[1]])
+    if self.board[move[0]][move[1]] != EMPTY:
+      raise InvalidMoveException("Position already taken")
 
-    self.board[move[0]][move[1]] = PLAYER_1_TOKEN if player == PLAYER_1 else PLAYER_2_TOKEN
+    self.board[move[0]][move[1]] = player
 
   def check_win(self):
-    #check rows
-    for board in [self.board, np.transpose(self.board)]:
-      for row in board:
-        if len(set(row)) == 1 and row[0] != EMPTY_TOKEN:
-          return PLAYER_1 if row[0] == PLAYER_1_TOKEN else PLAYER_2
+    #check rows and for empty
+    has_empty = False
+    for row in self.board:
+      temp_set = set(row)
+      if len(temp_set) == 1 and row[0] != EMPTY:
+        return row[0]
+      if not has_empty and EMPTY in temp_set:
+        has_empty = True
 
-    if len(set([board[i][i] for i in xrange(len(board))])) == 1 and board[0][0] != EMPTY_TOKEN:
-      return PLAYER_1 if board[0][0] == PLAYER_1_TOKEN else PLAYER_2
+    #check columns
+    for row in np.transpose(self.board):
+      if len(set(row)) == 1 and row[0] != EMPTY:
+        return row[0]
 
-    if len(set([board[i][len(board) - i - 1] for i in xrange(len(board))])) == 1 and board[0][len(board) - 1] != EMPTY_TOKEN:
-      return PLAYER_1 if board[0][len(board) - 1] == PLAYER_1_TOKEN else PLAYER_2
+    #check diagonals
+    if len(set([self.board[i][i] for i in xrange(len(self.board))])) == 1 and self.board[0][0] != EMPTY:
+      return self.board[0][0]
+
+    if len(set([self.board[i][len(self.board) - i - 1] for i in xrange(len(self.board))])) == 1 and self.board[0][len(self.board) - 1] != EMPTY:
+      return self.board[0][len(self.board) - 1]
+
+    if not has_empty:
+      return CAT_GAME
+
+    return NO_WIN
 
 
 
